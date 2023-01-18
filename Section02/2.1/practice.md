40803980b2da5d82634c129df166a903ad68eefd
tylergrez <tyler.grzegorek@gmail.com>
2023-01-17 20:43:39 -0700

    Seciton 2 up to 2.2

---
+++ Section02/2.1/practice.md
@@ -0,0 +1,31 @@
+# this is the first level header
+## this is the second level
+### third level
+#### fourth level
+##### fifth level
+###### sixth level
+_this text is in italics_
+**this text is bold**
+**_this text is both_**
+<br>
+sample text
+<br>
+~~this text is strikethrough~~
+<br>
+- list item 1 <br>
+- list item 2 <br>
+- list item 3 <br>
+1. ordered list 1 <br>
+2. ordered list 2 <br>
+3. ordered list 3 <br>
+---
+1. 1
+2. 2
+3. 3
+
+### This is my sample paragraph ###
+**The Beginning** <br>
+I am writing this paragraph simply to practice my markdown language. Other things I need to practice:
+- More coding
+- More coding still
+- And even more coding <br>
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,26 @@
+// create a constant variable (const)
+const const1 = "constant variable";
+// create a variable that can be reassigned (let)
+let let1 = "variable that can be reassigned";
+// create three variables and assign them values of different data types (=)
+let number = 1;
+let letter = "a";
+let boolean = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof number);
+console.log(typeof letter);
+console.log(typeof boolean);
+// change the value referenced by a variable (dynamic typing)
+letter = 8;
+// print the type of the variable you just changed (typeof)
+console.log(typeof letter);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+const myName = "Tyler";
+const language = "javascript";
+let weeks = 2;
+let myStatus = false;
+
+console.log("Hello, my name is " + myName + ", and I am learning " + language + ". I have been practicing for only " +  weeks + " weeks, so my status as a master is: " + myStatus + ".");
